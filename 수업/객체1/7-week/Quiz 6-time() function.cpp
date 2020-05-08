e948d5934dc57faf1e46fdcc0a1ad0bfafc43155
kang hyeon <62539341+Hyeon3356@users.noreply.github.com>
2020-05-08 11:45:30 +0900

    time function upload

--- 수업/객체1/7-week/Quiz 6-time() function.cpp
+++ 수업/객체1/7-week/Quiz 6-time() function.cpp
@@ -1,7 +1,7 @@
 /*
 time() 함수를 사용하여 현재 시간을 출력하는 프로그램을 작성하세요.
 1초에 한번씩 현재 시간을 출력
-sleep(1000)은 1000밀리세컨드 대기하는 함수.
+Sleep(1000)은 1000밀리세컨드 대기하는 함수.
 출력형태 : 13시 5분 10초
 못 풀음.
 */
@@ -12,7 +12,18 @@ sleep(1000)은 1000밀리세컨드 대기하는 함수.
 using namespace std;
 
 int main(){
-    while(!cin.eof()){
-        _sleep(1000);
+    while(true){
+        long int mytime = (long int)time(0);
+        mytime %= (60*60*24);
+        int hour, min, sec;
+        hour = mytime / (60*60);
+        mytime %= (60*60);
+        min = mytime / 60;
+        sec = mytime % 60;
+        (hour + 9 > 24 ? hour = hour + 9 - 24 : hour = hour + 9);
+
+        cout << hour << "h " << min << "min " << sec << "sec" << "\r";
+        Sleep(1000);
     }
+    return 0;
 }
